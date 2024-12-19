dc8642d156d2eff0a799d86ccd9cca9fde03fdd1
rafat21 <0092powerstar@gmail.com>
2024-12-19 23:32:34 +0600

    base

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+
+  return (
+    <>
+      <h1>My Favourite Color Is Black</h1>
+      <button>Black</button>     
+    </>
+  )
+}
+
+export default App

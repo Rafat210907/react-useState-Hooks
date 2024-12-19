5468105d192b549e157d6a59324a9f1288dc4454
rafat21 <0092powerstar@gmail.com>
2024-12-19 23:45:48 +0600

    simple example of useState hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import './App.css'
+import './App.css' // Importing a CSS file for styling the application
+import {useState} from "react"; // Importing the useState hook from React to manage state
 
+// Defining the main App component
 function App() {
 
+  // Declaring a state variable 'click' with initial value 'black'
+  // setColor is the function to update the state
+  const [click, setColor] = useState('black')
 
-
+  // Defining a function to change the state value of 'click' to 'red'
+  const changeColor = () => {
+    setColor('red') // Updates the state to 'red'
+  }
+  
   return (
     <>
-      <h1>My Favourite Color Is Black</h1>
-      <button>Black</button>     
+      {/* Displaying the current state value in the heading */}
+      <h1>My Favourite Color Is {click}</h1>
+      
+      {/* Button to trigger the changeColor function on click */}
+      <button onClick={changeColor}>color</button>     
     </>
   )
 }
-
+// Exporting the App component to be used in other parts of the application
 export default App
